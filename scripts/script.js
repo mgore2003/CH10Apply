e0d7a403149c503db3244fe9e4e8be7b19bc39cb
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-01-11 07:59:29 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/*
+	Student Name: Mark Gore
+	File Name: script.js
+	Date: 01/11/2024
+*/
+
+// Function to display content.
+function content() {
+	var text = document.getElementById("new");
+	text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+	text.style.color = "#c0145f";
+	text.style.fontSize = "2em";
+}
